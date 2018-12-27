2cac430f294e584c59de76f45cc1ae3aec2e40f0
kamilmoskal <kamil_moskal@interia.pl>
2018-12-27 14:45:13 +0100

    Updates

---
+++ precache-manifest.8825fde81c8e726a29daff79e67717ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12aaa566fc9a50f4560b29ac55cbde3a",
+    "url": "/punkbeer-appi/static/media/star.12aaa566.png"
+  },
+  {
+    "revision": "f5c2959bdaa0e23c31b6",
+    "url": "/punkbeer-appi/static/js/runtime~main.f5c2959b.js"
+  },
+  {
+    "revision": "a544a8d1e0f2545c2bd0",
+    "url": "/punkbeer-appi/static/js/main.a544a8d1.chunk.js"
+  },
+  {
+    "revision": "e7564679978bde17867f",
+    "url": "/punkbeer-appi/static/js/1.e7564679.chunk.js"
+  },
+  {
+    "revision": "a544a8d1e0f2545c2bd0",
+    "url": "/punkbeer-appi/static/css/main.ed5bdf7a.chunk.css"
+  },
+  {
+    "revision": "5f9501fbbf336cf56d0d219f06a9bed1",
+    "url": "/punkbeer-appi/index.html"
+  }
+];
